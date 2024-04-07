ec2bb9660d614d8a81e5bf8ad59d25144d652043
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2024-04-07 23:10:07 +0500

    formik hook is added

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,54 +1,71 @@
 import { Link, useNavigate } from "react-router-dom";
 import "./Add.scss";
-import { useState } from "react";
 import axios from "axios";
 import { Button, MenuItem, TextField } from "@mui/material";
 import { GroupAdd, KeyboardBackspace } from "@mui/icons-material";
+import { useFormik } from "formik";
+
 const Add = () => {
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    group: "",
-    age: "All",
-  });
-  const postStudent = async () => {
-    if (
-      formData.firstName.length > 3 &&
-      formData.lastName.length > 0 &&
-      formData.age.length <= 2
-    ) {
-      try {
-        await axios.post("http://localhost:3000/students", formData);
-      } catch (error) {
-        console.log(error.message, "hatolik bor");
-      }
-      navigate("/");
-    } else if (formData) {
-      alert("Please field your information before 😜😜😜");
-    }
-  };
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      group: "",
+      age: "All",
+    },
+    validate: (values) => {
+      const errors = {};
+      if (values.firstName === "") {
+        errors.firstName = "First name is required !!!";
+      }
+      if (values.lastName === "") {
+        errors.lastName = " Last name is required !!!";
+      }
+      if (values.group === "") {
+        errors.group = " Group is required !!!";
+      }
+      if (values.age === "") {
+        errors.age = " Age is required !!! ";
+      }
+      return errors;
+    },
+    onSubmit: async (values) => {
+      if (values.firstName.length > 3 && values.lastName.length > 0) {
+        try {
+          await axios.post("http://localhost:3000/students", values);
+          navigate("/students"); 
+        } catch (error) {
+          console.log(error.message, "hatolik bor");
+        }
+      } else {
+        alert("Please field your information before 😜😜😜");
+      }
+    },
+  });
+
+  const { values, handleChange, handleSubmit, handleBlur, errors, touched } =
+    formik;
 
   return (
     <div className="add">
       <div className="container">
         <div className="add_head">
-          <Link to="/">
+          <Link to="/students">
             <Button color="success" variant="contained">
               <KeyboardBackspace sx={{ fontSize: "35px" }} />
             </Button>
           </Link>
           <span>Add student</span>
         </div>
-        <div className="add_body">
+        <form
+          className="add_body"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
           <div className="add_info">
             <div className="input">
               <TextField
@@ -59,9 +76,12 @@ const Add = () => {
                 id="firstName"
                 size="medium"
                 sx={{ width: "300px" }}
-                value={formData.firstName}
-                required
+                value={values.firstName}
+                onBlur={handleBlur}
               />
+              <span className="err">
+                {touched.firstName && errors.firstName && errors.firstName}
+              </span>
             </div>
             <div className="input">
               <TextField
@@ -71,10 +91,13 @@ const Add = () => {
                 type="text"
                 size="medium"
                 sx={{ width: "300px" }}
-                value={formData.lastName}
-                required
+                value={values.lastName}
+                onBlur={handleBlur}
                 id="lastName"
               />
+              <span className="err">
+                {touched.lastName && errors.lastName && errors.lastName}
+              </span>
             </div>
             <div className="input">
               <TextField
@@ -84,10 +107,13 @@ const Add = () => {
                 type="number"
                 size="medium"
                 sx={{ width: "300px" }}
-                value={formData.age}
-                required
+                value={values.age}
+                onBlur={handleBlur}
                 id="age"
               />
+              <span className="err">
+                {touched.age && errors.age && errors.age}
+              </span>
             </div>
             <div className="input">
               <TextField
@@ -95,43 +121,28 @@ const Add = () => {
                 label="Group"
                 onChange={handleChange}
                 name="group"
-                value={formData.group}
+                value={values.group}
                 size="medium"
                 id="group"
                 sx={{ width: "300px" }}
-                required
+                onBlur={handleBlur}
               >
                 <MenuItem value="All">All</MenuItem>
                 <MenuItem value="N45">N45</MenuItem>
                 <MenuItem value="N50">N50</MenuItem>
                 <MenuItem value="N38">N38</MenuItem>
               </TextField>
+              <span className="err">
+                {touched.group && errors.group && errors.group}
+              </span>
             </div>
-
-            {/* <div className="info">
-              <label htmlFor="group">Group</label>
-              <select
-                name="group"
-                value={formData.group}
-                onChange={handleChange}
-                id="group"
-                required={formData.group}
-              >
-                <option value="All">All</option>
-                <option value="">N45</option>
-                <option value="">N50</option>
-                <option value="">N38</option>
-              </select>
-            </div> */}
           </div>
           <div className="btn">
-            <Link onClick={postStudent}>
-              <Button variant="contained" color="success">
-                <GroupAdd sx={{ fontSize: "40px" }} />
-              </Button>
-            </Link>
+            <Button type="submit" variant="contained" color="success">
+              <GroupAdd sx={{ fontSize: "40px" }} />
+            </Button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
--- src/pages/AddTeacher.jsx
+++ src/pages/AddTeacher.jsx
@@ -1,54 +1,71 @@
 import { Link, useNavigate } from "react-router-dom";
 import "./Add.scss";
-import { useState } from "react";
+// import { useState } from "react";
 import axios from "axios";
 import { Button, MenuItem, TextField } from "@mui/material";
 import { GroupAdd, KeyboardBackspace } from "@mui/icons-material";
+import { useFormik } from "formik";
 const AddTeacher = () => {
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    group: "",
-    age: "All",
-  });
-  const postStudent = async () => {
-    if (
-      formData.firstName.length > 3 &&
-      formData.lastName.length > 0 &&
-      formData.age.length <= 2
-    ) {
-      try {
-        await axios.post("http://localhost:3000/teacher", formData);
-      } catch (error) {
-        console.log(error.message, "hatolik bor");
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      group: "",
+      age: "",
+    },
+    validate: (values) => {
+      let errors = {};
+      if (values.firstName === "") {
+        errors.firstName = "First Name is required !!!";
       }
-      navigate("/");
-    } else if (formData) {
-      alert("Please field your information before 😜😜😜");
-    }
-  };
+      if (values.lastName === "") {
+        errors.lastName = "Last Name is required !!!";
+      }
+      if (values.group === "") {
+        errors.group = "Group is required !!!";
+      }
+      if (values.age === "") {
+        errors.age = "Age is required !!! ";
+      }
+      return errors;
+    },
+    onSubmit: async (values) => {
+      if (values.firstName.length > 3 && values.lastName.length > 0) {
+        try {
+          await axios.post("http://localhost:3000/teacher", values);
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+          navigate("/");
+        } catch (error) {
+          console.log(error.message, "hatolik bor");
+        }
+      } else {
+        alert("Please fill in your information before 😜😜😜");
+      }
+    },
+  });
+
+  const { values, handleChange, handleSubmit, handleBlur, errors, touched } =
+    formik;
 
   return (
     <div className="add">
       <div className="container">
         <div className="add_head">
-          <Link to="/teacher">
+          <Link to="/">
             <Button color="success" variant="contained">
               <KeyboardBackspace sx={{ fontSize: "35px" }} />
             </Button>
           </Link>
           <span>Add Teacher</span>
         </div>
-        <div className="add_body">
+        <form
+          className="add_body"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
           <div className="add_info">
             <div className="input">
               <TextField
@@ -58,10 +75,13 @@ const AddTeacher = () => {
                 type="text"
                 size="medium"
                 sx={{ width: "300px" }}
-                value={formData.firstName}
-                required
+                value={values.firstName}
+                onBlur={handleBlur}
                 id="firstName"
               />
+              <span className="err">
+                {touched.firstName && errors.firstName && errors.firstName}
+              </span>
             </div>
             <div className="input">
               <TextField
@@ -72,9 +92,12 @@ const AddTeacher = () => {
                 id="lastName"
                 size="medium"
                 sx={{ width: "300px" }}
-                value={formData.lastName}
-                required
+                value={values.lastName}
+                onBlur={handleBlur}
               />
+              <span className="err">
+                {touched.lastName && errors.lastName && errors.lastName}
+              </span>
             </div>
             <div className="input">
               <TextField
@@ -85,9 +108,12 @@ const AddTeacher = () => {
                 id="age"
                 size="medium"
                 sx={{ width: "300px" }}
-                value={formData.age}
-                required
+                value={values.age}
+                onBlur={handleBlur}
               />
+              <span className="err">
+                {touched.age && errors.age && errors.age}
+              </span>
             </div>
             <div className="input">
               <TextField
@@ -96,26 +122,26 @@ const AddTeacher = () => {
                 onChange={handleChange}
                 name="group"
                 size="medium"
-                value={formData.group}
+                value={values.group}
                 sx={{ width: "300px" }}
-                required
+                onBlur={handleBlur}
                 id="group"
               >
-                <MenuItem value="All">All</MenuItem>
                 <MenuItem value="junior">Junior</MenuItem>
                 <MenuItem value="middle">Middle</MenuItem>
                 <MenuItem value="senior">Senior</MenuItem>
               </TextField>
+              <span className="err">
+                {touched.group && errors.group && errors.group}
+              </span>
             </div>
           </div>
           <div className="btn">
-            <Link onClick={postStudent}>
-              <Button variant="contained" color="success">
-                <GroupAdd sx={{ fontSize: "40px" }} />
-              </Button>
-            </Link>
+            <Button type="submit" variant="contained" color="success">
+              <GroupAdd sx={{ fontSize: "40px" }} />
+            </Button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
